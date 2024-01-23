269a4cff1651206236bcc36a9f582e69b1096c96
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2024-01-23 13:29:45 -0800

    posts based on user id

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,28 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, PostCard } from '../components'
 import appwriteService from "../appwrite/config";
-
+import { useSelector } from 'react-redux'
 function AllPosts() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {}, [])
-    appwriteService.getPosts([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
+    const [loading, setLoading] = useState(false)
+    const auth = useSelector(state => state.auth)
+
+    useEffect(() => {
+        try {
+            appwriteService.getCurrentUserPosts(auth.userData.$id).then((posts) => {
+                if (posts) {
+                    setPosts(posts.documents)
+                }
+                setLoading(true)
+            })
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false)
         }
-    })
-  return (
-    <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-                {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard {...post} />
-                    </div>
-                ))}
-            </div>
+    }, [])
+
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                {!loading ? 'loading..' : <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>}
             </Container>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default AllPosts
