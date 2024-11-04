caef57decd1a7827b03ebfc407b0a1863ba7dfc6
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2024-11-04 06:57:14 -0800

    add changes in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,26 +3,28 @@ import appwriteService from "../appwrite/config";
 import { Container, PostCard } from "../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-
+import parse from "html-react-parser";
+import ArrowIcons from "../assets/Arrow";
 function Home() {
   const [posts, setPosts] = useState([]);
   const searchTerm = useSelector((state) => state.search);
 
-
   function formatDateWithDayName(timestamp) {
-     const date = new Date(timestamp);
+    const date = new Date(timestamp);
 
     // Get day name using Intl.DateTimeFormat
-    const dayName = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
+    const dayName = new Intl.DateTimeFormat("en-US", {
+      weekday: "long",
+    }).format(date);
 
     // Extract year, month, and day
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
-    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, "0");
 
     // Return formatted date as Day, YYYY-MM-DD
     return `${dayName}, ${year}-${month}-${day}`;
-}
+  }
 
   useEffect(() => {
     if (!searchTerm) {
@@ -39,8 +41,6 @@ function Home() {
       );
     }
   }, [searchTerm]);
-  console.log(posts,'posts==');
-  
 
   if (posts.length === 0)
     <div className="w-full py-8 mt-4 text-center">
@@ -65,36 +65,114 @@ function Home() {
       <Container>
         <p className="text-2xl font-semibold py-10">Recent blog posts</p>
         <div className="flex flex-wrap">
-          {posts.slice(7,8).map((post) => (
+          {posts.slice(7, 8).map((post) => (
             <div key={post.$id} className="p-2">
-              <Link to={`/post/${post.$id}`}>
-                <div className="   rounded-xl p-4">
-                  <div className=" max-w-7xl w-full justify-center mb-4">
-                    <img
-                      src={appwriteService.getFilePreview(post.featuredImage)}
-                      alt={post.title}
-                      className="rounded-lg object-cover w-[1200px] min-w-7xl h-96 "
-                    />
-                  </div>
-                  <h2 className="text-xl font-bold">{post.title}</h2>
-                  <h2 className="text-xl font-bold">{formatDateWithDayName(post.$createdAt)}</h2>
+              <div className="   rounded-xl p-4">
+                <div className=" max-w-7xl w-full justify-center mb-4">
+                  <img
+                    src={appwriteService.getFilePreview(post.featuredImage)}
+                    alt={post.title}
+                    className="rounded-lg object-contain  md:object-cover w-full  md:w-[1200px]   h-96 "
+                  />
                 </div>
-              </Link>
+                <h2 className="text-xl font-medium text-text-purple py-3">
+                  {formatDateWithDayName(post.$createdAt)}
+                </h2>
+                <div className="flex justify-between">
+                  <h2 className="text-4xl  font-medium py-5 items-center">
+                    {post.title}
+                  </h2>
+                  <Link to={`/post/${post.$id}`}>
+                    {/* <img src={ArrowIcon} className="h-4 w-4"/> */}
+                    <ArrowIcons />
+                  </Link>
+                </div>
+                <h2 className="text-2xl   py-5 text-gray-500 pb-7">
+                  {parse(post.content)}
+                </h2>
+                <span className="bg-text-purple/10 p-1 rounded-xl  text-text-purple font-medium px-2">
+                  {post.categories}
+                </span>
+              </div>
             </div>
           ))}
         </div>
       </Container>
+
+
       <div className="w-full py-8">
         <Container>
           <div className="flex flex-wrap">
-            {posts.map((post) => (
-              <div key={post.$id} className="p-2 w-1/4">
-                <PostCard {...post} />
+            {posts.slice(0, 2).map((post) => (
+              <div key={post.$id} className="p-2 w-full ">
+                <Link to={`/post/${post.$id}`}>
+                  <div className="w-full flex-col md:flex-row rounded-xl p-4 flex">
+                    <div className="w-full md:w-3/6  justify-center mb-4">
+                      <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-xl h-80 object-cover w-11/12"
+                      />
+                    </div>
+                    <div className="w-full md:w-4/12">
+                      <h2 className="text-xl font-medium text-text-purple py-3">
+                        {formatDateWithDayName(post.$createdAt)}
+                      </h2>
+
+                      <h2 className="text-2xl font-medium">{post.title}</h2>
+                      <h2 className="text-xl   py-5 text-gray-500 pb-7">
+                        {parse(post.content.substring(0, 50))}
+                      </h2>
+                      <span className="bg-text-purple/10 p-1 rounded-xl  text-text-purple font-medium px-2">
+                        {post.categories}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
               </div>
             ))}
           </div>
         </Container>
       </div>
+
+
+
+      <div className="w-full py-8">
+        <Container>
+        <p className="text-2xl font-semibold py-10">All blog posts</p>
+          <div className="flex flex-wrap">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-full md:w-6/12">
+                <Link to={`/post/${post.$id}`}>
+                  <div className="w-full flex-col  rounded-xl p-4 flex">
+                    <div className="w-full    mb-4">
+                      <img
+                        src={appwriteService.getFilePreview(post.featuredImage)}
+                        alt={post.title}
+                        className="rounded-xl h-80 object-contain w-11/12"
+                      />
+                    </div>
+                    <div className="w-full  ">
+                      <h2 className="text-xl font-medium text-text-purple py-3">
+                        {formatDateWithDayName(post.$createdAt)}
+                      </h2>
+
+                      <h2 className="text-2xl font-medium">{post.title}</h2>
+                      <h2 className="text-xl   py-5 text-gray-500 pb-7">
+                        {parse(post.content.substring(0, 50))}
+                      </h2>
+                      <span className="bg-text-purple/10 p-1 rounded-xl  text-text-purple font-medium px-2">
+                        {post.categories}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+      
     </div>
   );
 }
