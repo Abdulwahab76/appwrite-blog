26b0849ad06ee253872571392994885253557071
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2024-11-06 05:15:36 -0800

    change style of pagination

--- src/components/PaginatedList.jsx
+++ src/components/PaginatedList.jsx
@@ -25,7 +25,7 @@ const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
     };
 
     return (
-        <div className="pagination">
+        <div className="pagination flex justify-between px-10 py-10">
             <button
                 onClick={goToPreviousPage}
                 disabled={currentPage === 1}
@@ -35,16 +35,19 @@ const Pagination = ({ totalItems, itemsPerPage, onPageChange }) => {
             </button>
 
             {/* Page number buttons */}
+            <div>
+
+            
             {Array.from({ length: totalPages }, (_, index) => (
                 <button
                     key={index + 1}
                     onClick={() => goToPage(index + 1)}
-                    className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}
+                    className={`pagination-button p-2  px-4  ${currentPage === index + 1 ? 'active !text-text-purple !bg-text-purple/10 !font-normal'  : ''}`}
                 >
                     {index + 1}
                 </button>
             ))}
-
+</div>
             <button
                 onClick={goToNextPage}
                 disabled={currentPage === totalPages}
