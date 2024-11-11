3329bfd8d73889e411fcf31c176c51984779f316
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2024-11-11 13:43:10 -0800

    added import

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { Login, Signup } from "../components";
+
+import About from "./About";
+import AddPost from "./AddPost";
+import AllPosts from "./AllPosts";
+import EditPost from "./EditPost";
+import Home from "./Home";
+import Post from "./Post";
+export { Signup, About, AddPost, AllPosts, EditPost, Home, Post };
